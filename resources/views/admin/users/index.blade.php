48aed3a7839b6f0db58ddb988fb05d1fcc2d57f1
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-01-14 17:58:08 +0200

    init commit:insert alert to delete

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -45,4 +45,31 @@
         </tbody>
     </table>
     {{ $users->links() }}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+
+    <script>
+        $('.btn-delete').on('click', function() {
+
+            let form = $(this).next('form');
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                }
+            })
+        });
+    </script>
 @stop
