3224bf3877864081f6edb345717db28420e7adfd
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-01-14 18:03:49 +0200

    init commit:search form

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -13,6 +13,12 @@
         </div>
     @endif
 
+    <form action="{{ route('admin.users.index') }}" method="get">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search here..." name="q" value="{{ request()->q }}">
+            <button class="btn btn-dark px-5" id="button-addon2">Search</button>
+        </div>
+    </form>
     <table class="table table-bordered">
         <thead>
             <tr class="bg-dark text-white">
