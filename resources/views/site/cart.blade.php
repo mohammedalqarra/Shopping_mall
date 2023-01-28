a6cb97edee64624f31b3e4c0260cd383a2571a32
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-01-28 22:31:58 +0200

    init:commit add commit ifviewchart

--- resources/views/site/cart.blade.php
+++ resources/views/site/cart.blade.php
@@ -27,44 +27,50 @@
                     <div class="col-md-8 col-md-offset-2">
                         <div class="block">
                             <div class="product-list">
-                                <form method="POST" action="{{ route('site.update_cart') }}">
-                                    @csrf
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th class="">Name</th>
-                                                <th class="">Price</th>
-                                                <th class="">Quantity</th>
-                                                <th class="">Totla</th>
-                                                <th class="">Actions</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        @php
-                                            $total = 0 ;
-                                        @endphp
-                                            @foreach (auth()->user()->carts as $cart)
-                                            <tr class="">
-                                                <td class="">
-                                                    <div class="product-info">
-                                                        <img width="80" src="{{ asset('uploads/products/'. $cart->product->image) }}"
-                                                            alt="">
-                                                        <a href="{{ route('site.product' , $cart->product->slug ) }}">{{ $cart->product->trans_name }}</a>
-                                                    </div>
-                                                </td>
-                                                <td class="">${{ $cart->price }}</td>
-                                                <td class=""><input type="number"  value="{{ $cart->quantity }}" name="qyt[{{ $cart->product_id }}]" style="width:50px"></td>
-                                                <td class="">${{ $cart->price * $cart->quantity }}</td>
-                                                <td class="">
-                                                    <a   class="product-remove btn-delete" href="{{ route('site.remove_cart' , $cart->id) }}">Remove</a>
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                    <button class="btn btn-solid-border"> Update Cart</button>
-                                    <a href="{{ route('site.checkout') }}" class="btn btn-main pull-right">Checkout</a>
-                                </form>
+                               @if (auth()->user()->carts->count() > 0)
+                               <form method="POST" action="{{ route('site.update_cart') }}">
+                                @csrf
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th class="">Name</th>
+                                            <th class="">Price</th>
+                                            <th class="">Quantity</th>
+                                            <th class="">Totla</th>
+                                            <th class="">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    @php
+                                        $total = 0 ;
+                                    @endphp
+                                        @foreach (auth()->user()->carts as $cart)
+                                        <tr class="">
+                                            <td class="">
+                                                <div class="product-info">
+                                                    <img width="80" src="{{ asset('uploads/products/'. $cart->product->image) }}"
+                                                        alt="">
+                                                    <a href="{{ route('site.product' , $cart->product->slug ) }}">{{ $cart->product->trans_name }}</a>
+                                                </div>
+                                            </td>
+                                            <td class="">${{ $cart->price }}</td>
+                                            <td class=""><input type="number"  value="{{ $cart->quantity }}" name="qyt[{{ $cart->product_id }}]" style="width:50px"></td>
+                                            <td class="">${{ $cart->price * $cart->quantity }}</td>
+                                            <td class="">
+                                                <a   class="product-remove btn-delete" href="{{ route('site.remove_cart' , $cart->id) }}">Remove</a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <button class="btn btn-solid-border"> Update Cart</button>
+                                <a href="{{ route('site.checkout') }}" class="btn btn-main pull-right">Checkout</a>
+                            </form>
+                                   @else
+                                   <div class="text-center">
+                                    <a href="{{ route('site.shop') }}" class="btn btn-main">Shop Now</a>
+                                   </div>
+                               @endif
                             </div>
                         </div>
                     </div>
