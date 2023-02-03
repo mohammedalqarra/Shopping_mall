8579d3d69001968f79eda89a29b58b892138e636
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-02-03 21:33:23 +0200

    init:commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,12 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+
+// $app = new Illuminate\Foundation\Application(
+//     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__),
+//     dirname(__DIR__) . DIRECTORY_SEPARATOR . 'envfolder'
+// );
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
